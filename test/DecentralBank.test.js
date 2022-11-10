5805f3a406c01c8e4fa627f31ec2512896953944
Dania001 <96878045+Dania001@users.noreply.github.com>
2022-11-10 20:18:15 +0530

    Update DecentralBank.test.js

--- test/DecentralBank.test.js
+++ test/DecentralBank.test.js
@@ -2,17 +2,11 @@ const Tether = artifacts.require("./Tether.sol");
 const Reward = artifacts.require("./Reward.sol");
 const DecentralBank = artifacts.require("./DecentralBank.sol");
 
-//require("chai");
-//.use(require("chai-as-promised"))
-//.should()
-
 var chai = require('chai');
-//var expect = chai.expect;
-//var assert = chai.assert;
 var chaiAsPromised = require("chai-as-promised");
 chai.use(chaiAsPromised);
 
-contract ("DecentralBank", ([owner, customer]) => {
+contract ("DecentralBank", ([owner, investor]) => {
 let tether, reward, decentralBank;
 
 function tokens(number){
@@ -28,22 +22,24 @@ decentralBank = await DecentralBank.new(reward.address, tether.address);
 //trnasfer all reward token to bank
 await reward.TransferReward(decentralBank.address,tokens("1000000"));
 
-//transfer 100 mocks tether to investor/customer
-await tether.transferMock(customer, tokens("100"), {from:owner});
+//transfer 100 mocks tether to investor
+await tether.transferMock(investor, tokens("100"), {from:owner});
 })
 
 describe("mock tether deployment", async () =>{
-it("should match the name successfully", async()=>{
+it("should match the name and symbol correctly", async()=>{
 const name = await tether.name()
+const symbol = await tether.symbol()
 assert.equal(name,"tether project")
+assert.equal(symbol,"USDT")
 })
 })
 describe("reward token deployment", async() =>{
-it("should matches the name correctly", async () =>{
-const name = await reward.new();
-//const symbol = await reward.new();
+it("should matches the name and symbol correctly", async () =>{
+const name = await reward.name();
+const symbol = await reward.symbol();
 assert.equal(name, "Reward Token")
-//assert.equal(symbol,"RWD")
+assert.equal(symbol,"RWD")
 })
 })
 describe("decentralBank deployment",async()=>{
@@ -56,4 +52,26 @@ let balance = await reward.BalanceOf(decentralBank.address)
 assert.equal(balance, tokens("1000000"))
 })
 })
+describe("staking", async() =>{
+it("give tether tokens for staking", async() =>{
+let result
+//check investor balance
+result = await tether.balanceOf(investor)
+assert.equal(result,tokens("100"),"investor mock balance before staking")
+
+////check staking for customer of 100 tokens
+await tether.approve(decentralBank.address, tokens("100"), {from: investor})
+await decentralBank.depositToken(tokens("100"), {from:investor})
+
+////updated balance after staking
+result = await tether.balanceOf(investor)
+assert.equal(result.toString(), tokens("0"), "investor balance after staking 100 mock")
+
+//////updated bank balance
+result = await tether.balanceOf(decentralBank.address)
+assert.equal(result, tokens("100"), "bank balance after staking 100 mock")
+
+})
+})
+
 })
