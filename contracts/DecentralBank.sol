a04bbd75b8a2691fa58974361a6ba96b8107592a
Dania001 <96878045+Dania001@users.noreply.github.com>
2022-11-10 20:14:24 +0530

    Update DecentralBank.sol

--- contracts/DecentralBank.sol
+++ contracts/DecentralBank.sol
@@ -9,8 +9,28 @@ contract DecentralBank{
     Tether public tether;
     Reward public reward;
 
+    address[] public stakers;
+    mapping(address => uint) public stakingBalance;
+    mapping(address => bool) public hasStaked;
+    mapping(address => bool) public isStaking;
+
     constructor(Reward _reward, Tether _tether) public{
         reward = _reward;
         tether = _tether;
+    }
+
+    function depositToken(uint _amount) public {
+        require(_amount > 0, "amount cannot be zero");
+//        transfer tether tokens to this address for staking
+        tether.transferFrom(msg.sender, address(this), _amount);
+
+//        update staking balance
+        stakingBalance[msg.sender] = stakingBalance[msg.sender] + _amount;
+        if(!hasStaked[msg.sender]) {
+            stakers.push(msg.sender);
+        }
+//        update stacking
+        hasStaked[msg.sender] = true;
+        isStaking[msg.sender] = true;
     }
 }
